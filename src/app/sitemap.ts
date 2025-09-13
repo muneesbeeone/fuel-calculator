c35549630613b1f024c8e284f8a7956177eca25d
Munees <muneesmmm@gmail.com>
2025-09-13 13:45:35 +0530

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { listCities } from '@/lib/fuelPrices'
 export const dynamic = "force-static"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://fuel-calculator.vercel.app' // Update this to your actual domain
+  const baseUrl = 'https://fuelcalculator.munees.co.in'
   
   // Static pages
   const staticPages = [
